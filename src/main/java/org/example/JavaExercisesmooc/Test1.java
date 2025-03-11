77daf79da0f6b8e619f72f052ad8e601acb3d355
Gentrithotii <Gentrithoti@hotmail.com>
2025-03-11 18:38:46 +0100

    Fixed Structure

--- src/main/java/org/example/JavaExercisesmooc/Test1.java
+++ src/main/java/org/example/JavaExercisesmooc/Test1.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.JavaExercisesmooc;
 
 import java.util.Scanner;
 
--- src/main/java/org/example/JavaFundamentals/Main.java
+++ src/main/java/org/example/JavaFundamentals/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.JavaFundamentals;
 
 import java.util.Random;
 import java.util.Scanner;
