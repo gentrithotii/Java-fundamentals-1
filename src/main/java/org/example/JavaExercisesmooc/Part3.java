0283b0daf1c72b344ed3cebe2c110152be132717
Gentrithotii <Gentrithoti@hotmail.com>
2025-03-11 22:43:57 +0100

    firstAndLastInTheList

--- src/main/java/org/example/JavaExercisesmooc/Part3.java
+++ src/main/java/org/example/JavaExercisesmooc/Part3.java
@@ -1,6 +1,7 @@
 package org.example.JavaExercisesmooc;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Part3 {
@@ -8,7 +9,41 @@ public class Part3 {
         Scanner userInput = new Scanner(System.in);
 
 //        thirdElementExercise(userInput);
-        secondPlusThird(userInput);
+//        secondPlusThird(userInput);
+//        generateIndexOutOfBounds();
+        firstAndLastInTheList(userInput);
+
+
+    }
+
+
+    public static void firstAndLastInTheList(Scanner sc) {
+        ArrayList<String> stringArrayList = new ArrayList<>();
+
+        while (true) {
+            System.out.print("Enter the string you want to add to the String ArrayList: ");
+            String textToBeStored = sc.nextLine();
+
+            if (textToBeStored.matches("")) {
+                sc.close();
+                break;
+            }
+            stringArrayList.add(textToBeStored);
+        }
+
+        int startIndex = 0, lastIndex = stringArrayList.size() - 1;
+        for (int i = 0; i < stringArrayList.size(); i++) {
+            if (i == startIndex) {
+                System.out.println(stringArrayList.get(i));
+            } else if (i == lastIndex) {
+                System.out.println(stringArrayList.get(i));
+            }
+        }
+    }
+
+    public static void generateIndexOutOfBounds() {
+        ArrayList<Integer> integerArrayList = new ArrayList<>(Arrays.asList(1, 2, 3));
+        integerArrayList.get(3);
     }
 
     public static void secondPlusThird(Scanner sc) {
@@ -46,4 +81,5 @@ public class Part3 {
         System.out.println(stringArrayList.get(2));
 
     }
+
 }
