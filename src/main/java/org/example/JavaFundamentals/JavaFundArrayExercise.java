efaa3d27597a7cf9828373620e714819b6757e62
Gentrithotii <Gentrithoti@hotmail.com>
2025-03-17 11:58:36 +0100

    Second exercise DONE

--- src/main/java/org/example/JavaFundamentals/JavaFundArrayExercise.java
+++ src/main/java/org/example/JavaFundamentals/JavaFundArrayExercise.java
@@ -6,12 +6,23 @@ import java.util.Scanner;
 public class JavaFundArrayExercise {
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
-        storeAndPrintIntArray(userInput);
+        int[] arrayNumbers = new int[0];
+        arrayNumbers = storeIntInArray(userInput, arrayNumbers);
+        printUserArray(arrayNumbers);
+        indexOfArrayNum(userInput, arrayNumbers);
     }
 
-    public static void storeAndPrintIntArray(Scanner userInput) {
-        int[] userNumbers = new int[0];
+    public static void indexOfArrayNum(Scanner userInput, int[] numberArray) {
+        System.out.print("Enter the number that your looking for to print the index of: ");
+        int userNumberToFind = userInput.nextInt();
+        for (int i = 0; i < numberArray.length; i++) {
+            if (numberArray[i] == userNumberToFind) {
+                System.out.println("Your number: " + numberArray[i] + " is at index of " + i);
+            }
+        }
+    }
 
+    public static int[] storeIntInArray(Scanner userInput, int[] userNumbers) {
         while (true) {
             System.out.print("Enter the number you want to add enter 0 if you want to quit:   ");
             int userNumber = userInput.nextInt();
@@ -21,12 +32,17 @@ public class JavaFundArrayExercise {
                 break;
             }
         }
-        System.out.print("Your numbers are:");
-        System.out.print(", ");
+        return userNumbers;
+    }
+
+    private static void printUserArray(int[] userNumbers) {
+        System.out.print("Your numbers are: ");
         for (int i = 0; i < userNumbers.length; i++) {
             System.out.print(userNumbers[i]);
+            System.out.print(", ");
             System.out.print(" ");
         }
+        System.out.println(" ");
     }
 
     public static int[] addOneIntToArray(int[] oldArray, int number) {
