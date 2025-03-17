b391c29938b882ed4fae893523d3f7d827eea82d
Gentrithotii <Gentrithoti@hotmail.com>
2025-03-17 11:11:13 +0100

    First Exercise on Array DONE

---
+++ src/main/java/org/example/JavaFundamentals/JavaFundArrayExercise.java
@@ -0,0 +1,39 @@
+package org.example.JavaFundamentals;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class JavaFundArrayExercise {
+    public static void main(String[] args) {
+        Scanner userInput = new Scanner(System.in);
+        storeAndPrintIntArray(userInput);
+    }
+
+    public static void storeAndPrintIntArray(Scanner userInput) {
+        int[] userNumbers = new int[0];
+
+        while (true) {
+            System.out.print("Enter the number you want to add enter 0 if you want to quit:   ");
+            int userNumber = userInput.nextInt();
+            if (userNumber != 0) {
+                userNumbers = addOneIntToArray(userNumbers, userNumber);
+            } else {
+                break;
+            }
+        }
+        System.out.println("Your numbers are:");
+        for (int i = 0; i < userNumbers.length; i++) {
+            System.out.print(userNumbers[i]);
+            System.out.println(" ");
+        }
+    }
+
+    public static int[] addOneIntToArray(int[] oldArray, int number) {
+        int[] newArray = Arrays.copyOf(oldArray, oldArray.length + 1);
+        newArray[newArray.length - 1] = number;
+
+        return newArray;
+    }
+
+}
+
