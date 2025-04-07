63da92173e261c741b56d38b7a2cf064f919428c
Gentrithotii <Gentrithoti@hotmail.com>
2025-04-07 23:07:17 +0200

    Second done

--- src/main/java/org/example/collectionexercises/CollectionExercises.java
+++ src/main/java/org/example/collectionexercises/CollectionExercises.java
@@ -1,5 +1,6 @@
 package org.example.collectionexercises;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -8,7 +9,7 @@ public class CollectionExercises {
     public static void main(String[] args) {
 
 //        firstExercise();
-
+        seconExecise();
 
     }
 
@@ -24,6 +25,21 @@ public class CollectionExercises {
 
         for (String s : daysOfWeek) {
             System.out.println(s);
+        }
+    }
+
+    public static void seconExecise() {
+        List<String> daysOfWeek = new ArrayList<>();
+        daysOfWeek.add("Monday");
+        daysOfWeek.add("Tuesday");
+        daysOfWeek.add("Wednesday");
+        daysOfWeek.add("Thursday");
+        daysOfWeek.add("Friday");
+        daysOfWeek.add("Saturday");
+        daysOfWeek.add("Sunday");
+
+        for (int i = 0; i < daysOfWeek.size(); i++) {
+            System.out.println(daysOfWeek.get(i));
         }
     }
 }
