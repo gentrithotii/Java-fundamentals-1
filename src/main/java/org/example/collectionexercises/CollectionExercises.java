a799d90d325f5eced42900181fe6216fe3b49d77
Gentrithotii <Gentrithoti@hotmail.com>
2025-04-08 15:22:56 +0200

    Fourth exercise done

--- src/main/java/org/example/collectionexercises/CollectionExercises.java
+++ src/main/java/org/example/collectionexercises/CollectionExercises.java
@@ -2,6 +2,7 @@ package org.example.collectionexercises;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.List;
 
@@ -10,7 +11,8 @@ public class CollectionExercises {
 
 //        firstExercise();
 //        seconExecise();
-        thirdExercise();
+//        thirdExercise();
+        fourthExercise();
 
     }
 
@@ -55,6 +57,31 @@ public class CollectionExercises {
         daysOfWeek.add("Thursday");
 
         for (String weekDays : daysOfWeek) {
+            System.out.println(weekDays);
+        }
+    }
+
+    public static void fourthExercise() {
+        List<String> daysOfWeek = new ArrayList<>();
+        daysOfWeek.add("Monday");
+        daysOfWeek.add("Tuesday");
+        daysOfWeek.add("Wednesday");
+        daysOfWeek.add("Thursday");
+        daysOfWeek.add("Friday");
+        daysOfWeek.add("Saturday");
+        daysOfWeek.add("Sunday");
+
+        System.out.println("Normal List");
+        System.out.println(" ");
+        for (String weekDays : daysOfWeek) {
+            System.out.println(weekDays);
+        }
+        System.out.println("--------------------");
+        System.out.println("Sublist");
+        System.out.println(" ");
+        List<String> subList = daysOfWeek.subList(0, 3);
+
+        for (String weekDays : subList) {
             System.out.println(weekDays);
         }
     }
