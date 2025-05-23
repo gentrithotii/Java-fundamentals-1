57e6b203096975fd9ab61d5df0fae600a3f04fa9
Gentrithotii <Gentrithoti@hotmail.com>
2025-05-23 15:10:22 +0200

    Part 5 DONE

--- src/main/java/org/example/JavaExercisesmooc/moocparts/Part5.java
+++ src/main/java/org/example/JavaExercisesmooc/moocparts/Part5.java
@@ -20,7 +20,56 @@ public class Part5 {
 //        testSongClassEquals();
 //        testPersonEquals();
 //        testBookExerciseEx2(userInput);
-        testSimpleDateAdvance();
+//        testSimpleDateAdvance();
+//        testMoney();
+//        testMoneyLessThen();
+        testReduceMoney();
+    }
+
+    private static void testReduceMoney() {
+        Money a = new Money(10, 0);
+        Money b = new Money(3, 50);
+
+        Money c = a.minus(b);
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 6.50e
+
+        c = c.minus(a);       // NB: a new Money object is created, and is placed "at the end of the strand connected to c"
+//  the old 6.5 euros at the end of the strand disappears and the Java garbage collector takes care of it
+
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 3.50e
+        System.out.println(c);  // 0.00e
+    }
+
+    private static void testMoneyLessThen() {
+        Money a = new Money(10, 0);
+        Money b = new Money(3, 0);
+        Money c = new Money(5, 0);
+
+        System.out.println(a.lessThan(b));
+        System.out.println(b.lessThan(c));
+    }
+
+    private static void testMoney() {
+        Money a = new Money(10, 0);
+        Money b = new Money(5, 0);
+
+        Money c = a.plus(b);
+
+        System.out.println(a);  // 10.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);  // 15.00e
+
+        a = a.plus(c);
+
+
+        System.out.println(a);  // 25.00e
+        System.out.println(b);  // 5.00e
+        System.out.println(c);  // 15.00e
     }
 
     private static void testSimpleDateAdvance() {
@@ -228,6 +277,65 @@ public class Part5 {
         }
     }
 
+}
+
+class Money {
+
+    private final int euros;
+    private final int cents;
+
+    public Money(int euros, int cents) {
+        this.euros = euros;
+        this.cents = cents;
+    }
+
+    public int euros() {
+        return euros;
+    }
+
+    public int cents() {
+        return cents;
+    }
+
+    public Money plus(Money addition) {
+        int totalCents = this.cents + addition.cents;
+        int totalEuros = this.euros + addition.euros + totalCents / 100;
+        int newCents = totalCents % 100;
+
+        return new Money(totalEuros, newCents);
+    }
+
+    public Money minus(Money decreaser) {
+        int euroResult = this.euros;
+        int centResult = this.cents;
+
+        euroResult -= decreaser.euros;
+        centResult -= decreaser.cents;
+
+        if (centResult < 0) {
+            euroResult--;
+            centResult += 100;
+        }
+
+        if (euroResult < 0) {
+            return new Money(0, 0);
+        }
+        return new Money(euroResult, centResult);
+    }
+
+    public boolean lessThan(Money compared) {
+        return euros() < compared.euros() || (euros() == compared.euros() && cents() < compared.cents());
+    }
+
+    @Override
+    public String toString() {
+        String zero = "";
+        if (cents <= 10) {
+            zero = "0";
+        }
+
+        return euros + "." + zero + cents + "e";
+    }
 }
 
 class SimpleDate {
